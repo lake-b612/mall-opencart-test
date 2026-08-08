df2ab60aec11d59d35e515b25839163db23a234c
lake-b612 <1400844996@qq.com>
2026-08-08 16:23:27 +0800

    Update user-register-check.sql

--- 04-mysql-check/user-register-check.sql
+++ 04-mysql-check/user-register-check.sql
@@ -1,1 +1,8 @@
+-- 查询全部注册用户
+SELECT customer_id, email, telephone, status FROM oc_customer;
 
+-- 统计注册用户数量
+SELECT count(*) AS user_count FROM oc_customer WHERE status = 1;
+
+-- 查询指定邮箱用户
+SELECT * FROM oc_customer WHERE email='test@demo.com';
