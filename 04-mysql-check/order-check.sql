1e74bd9d187cf85d488505bd2a191ee5373756f4
lake-b612 <1400844996@qq.com>
2026-08-08 16:23:06 +0800

    Update order-check.sql

--- 04-mysql-check/order-check.sql
+++ 04-mysql-check/order-check.sql
@@ -1,1 +1,11 @@
+-- 查询全部订单
+SELECT * FROM oc_order;
 
+-- 根据订单号查询订单详情
+SELECT * FROM oc_order WHERE order_id = 1;
+
+-- 查询订单对应的商品明细
+SELECT * FROM oc_order_product WHERE order_id = 1;
+
+-- 查询商品库存
+SELECT product_id, model, quantity FROM oc_product;
